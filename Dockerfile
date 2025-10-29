c16cedcbaeb27e9ad9ece22fd60f7d3058db1715
Mathias Muenscher <mmsec@t101.de>
2025-10-30 01:46:45 +0200

    Refactor to support only one encrypted dir and passfile

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,10 @@ RUN go install github.com/rfjakob/gocryptfs/v2@${GOCRYPTFS_VERSION}
 FROM alpine:latest
 
 ENV MOUNT_OPTIONS="-allow_other -nosyslog" \
-    UNMOUNT_OPTIONS="-u -z"
+    UNMOUNT_OPTIONS="-u -z" \
+    ENC_PATH="/external/encrypted" \
+    DEC_PATH="/external/decrypted" \
+    PWD_FILE="/pwd_file"
 
 COPY --from=builder /go/bin/gocryptfs /usr/local/bin/gocryptfs
 RUN apk --no-cache add fuse bash
--- run.sh
+++ run.sh
@@ -1,50 +1,82 @@
 #!/bin/bash
 set -e
 
-ENC_PATH=/encrypted
-DEC_PATH=/decrypted
-
-ENC_FOLDERS=`find ${ENC_PATH} ! -path ${ENC_PATH} -maxdepth 1 -type d`
-
 function sigterm_handler {
+  echo
   echo "sending SIGTERM to child pid"
-  kill -SIGTERM ${pids[@]}
+  if [[ -n "${child_pid:-}" ]]; then
+    kill -SIGTERM "$child_pid" 2>/dev/null || true
+    wait "$child_pid" || true
+  fi
   fuse_unmount
   echo "exiting container now"
   exit $?
 }
 
 function sighup_handler {
+  echo
   echo "sending SIGHUP to child pid"
-  kill -SIGHUP ${pids[@]}
-  wait ${pids[@]}
+  if [[ -n "${child_pid:-}" ]]; then
+    kill -SIGHUP "$child_pid" 2>/dev/null || true
+    wait "$child_pid" || true
+  fi
 }
 
 function fuse_unmount {
-  DEC_FOLDERS=`find ${DEC_PATH} ! -path ${DEC_PATH} -maxdepth 1 -type d`
-
-  for DEC_FOLDER in $DEC_FOLDERS; do
-    echo "Unmounting: fusermount $UNMOUNT_OPTIONS $DEC_FOLDER at: $(date +%Y.%m.%d-%T)"
-    fusermount $UNMOUNT_OPTIONS $DEC_FOLDER
-    rmdir $DEC_FOLDER
-  done
+  echo
+  echo "Unmounting: fusermount $UNMOUNT_OPTIONS $DEC_PATH at: $(date +%Y.%m.%d-%T)"
+  fusermount $UNMOUNT_OPTIONS "$DEC_PATH" || true
+  rmdir "$DEC_PATH" 2>/dev/null || true
 }
 
 trap sigterm_handler SIGINT SIGTERM
 trap sighup_handler SIGHUP
 
-unset pids
-for ENC_FOLDER in $ENC_FOLDERS; do
-  DEC_FOLDER=`echo "$ENC_FOLDER" | sed "s|^${ENC_PATH}|${DEC_PATH}|g"`
-  mkdir -p $DEC_FOLDER
-
-  if [ ! -f "${ENC_FOLDER}/gocryptfs.conf" ]; then
-    gocryptfs -init -extpass 'printenv PASSWD' $ENC_FOLDER
-  fi
-
-  gocryptfs $MOUNT_OPTIONS -fg -extpass 'printenv PASSWD' $ENC_FOLDER $DEC_FOLDER & pids+=($!)
-done
-wait "${pids[@]}"
+echo
+echo "Starting gocryptfs at: $(date +%Y.%m.%d-%T)"
+echo
+echo "Parameters:"
+echo "MOUNT_OPTIONS: $MOUNT_OPTIONS" 
+echo "UNMOUNT_OPTIONS: $UNMOUNT_OPTIONS" 
+echo "ENC_PATH: $ENC_PATH" 
+echo "DEC_PATH: $DEC_PATH" 
+echo "PWD_FILE: $PWD_FILE"
+echo
+echo "Checks:"
+if [ -f "$PWD_FILE" ]; then
+  echo "Password file found at: $PWD_FILE"
+else
+  echo "Password file missing at: $PWD_FILE"
+  exit 1
+fi
+if [ -d "$ENC_PATH" ]; then
+  echo "Encrypted path found at: $ENC_PATH"
+else
+  echo "Encrypted path missing at: $ENC_PATH"
+  echo "Will create path."
+  mkdir -p "$ENC_PATH"
+fi
+if [ -d "$DEC_PATH" ]; then
+  echo "Plain path found at: $DEC_PATH"
+else
+  echo "Plain path missing at: $DEC_PATH"
+  echo "Will create path."
+  mkdir -p "$DEC_PATH"
+fi
+if [ ! -f "${ENC_PATH}/gocryptfs.conf" ]; then
+  echo "gocryptfs filesystem not found at: ${ENC_PATH}"
+  echo
+  echo "Will initialize new gocryptfs filesystem."
+  gocryptfs -init -passfile "$PWD_FILE" "$ENC_PATH"
+  echo
+else
+  echo "gocryptfs filesystem found at: ${ENC_PATH}"
+fi
+echo
+echo "Mounting gocryptfs filesystem now."
+gocryptfs $MOUNT_OPTIONS -fg -passfile "$PWD_FILE" "$ENC_PATH" "$DEC_PATH" & child_pid=$!
+echo
+wait "$child_pid"
 
 echo "gocryptfs crashed at: $(date +%Y.%m.%d-%T)"
 fuse_unmount
